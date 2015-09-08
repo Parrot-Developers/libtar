7f55bd504c66adcc88419af67241aa9291795362
Ronan Chauvin <ronan.chauvin@parrot.com>
2015-09-08 17:10:50 +0200

    [BUILD] Add atom.mk

---
+++ atom.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libtar
+LOCAL_DESCRIPTION := tar library
+LOCAL_CATEGORY_PATH := libs
+
+LOCAL_EXPORT_LDLIBS += \
+	-ltar
+
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
+	--without-zlib
+
+include $(BUILD_AUTOTOOLS)
